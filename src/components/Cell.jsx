76f467095b2a35d1ecce71e20a41bf8a2db9df33
nasimkuniyil <md.nasimkuniyil@gmail.com>
2025-07-31 12:04:54 +0530

    refactor

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -4,7 +4,7 @@ export default function Cell({ onclick, cellId }) {
   const [value, setValue] = useState("");
 
   return (
-    <div className="bg-gray-200 rounded-xl flex items-center justify-center w-16 h-16 lg:hover:cursor-pointer lg:hover:bg-gray-100 lg:hover:scale-105 transition-all duration-300 " onClick={() => !value && onclick(setValue, cellId)}>
+    <div className="bg-gray-200 rounded-xl flex items-center justify-center w-16 lg:w-20 h-16 lg:h-20 lg:hover:cursor-pointer lg:hover:bg-gray-100 lg:hover:scale-105 transition-all duration-300 " onClick={() => !value && onclick(setValue, cellId)}>
       {value && (<h2 className="animate-popup-150">{value}</h2>)}
     </div>
   );
